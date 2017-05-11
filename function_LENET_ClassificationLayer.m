1bd50f5bb65b6d65610e13bc23bb5eb09e38d4b0
WyNfee <wangyifei52199@sina.com>
2017-05-11 17:28:04 +0800

    fix problems coz by project reconfiguration before

--- function_LENET_ClassificationLayer.m
+++ function_LENET_ClassificationLayer.m
@@ -1,4 +1,4 @@
 function r_prediction = function_LENET_ClassificationLayer(p_input_data, p_answer)
-    t_softmax = function_Utils_Softmax(p_input_data);
+    t_softmax = function_Utils_Softmax_Function(p_input_data);
     r_prediction = t_softmax(p_answer);
 end
