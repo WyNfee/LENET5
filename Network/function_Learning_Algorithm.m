ff0a46ac9afc9ae61da5209fde4b31fb166fd52b
WyNfee <wangyifei52199@sina.com>
2017-05-17 09:56:40 +0800

    Change the complex variable name to simple ones

--- Network/function_Learning_Algorithm.m
+++ Network/function_Learning_Algorithm.m
@@ -6,32 +6,30 @@
 %r_learnt_weight: the weight learnt in this training data set
 %r_cost_history: the cost history used to plot learning performance
 %r_network_struct: the information about this learning algorithm
-function[r_learnt_weight, r_cost_history, r_network_struct] = function_Learning_Algorithm(p_training_data, p_training_answer)
+function[r_learnt_weight, r_cost_history, r_network_struct] = function_Learning_Algorithm(p_training_data, p_y)
     %set up architecture parameters
-    t_layer_input_amount = 400;
-    t_layer_hidden_amount = 100;
-    t_layer_output_amount = 10;
+    t_n_input = 400;
+    t_n_fc_hidden = 100;
+    t_n_output = 10;
     
     %reorganize the answer data
-    t_training_data_amount = size(p_training_answer, 1);
+    t_n_all_data = size(p_y, 1);
     
     %init the weight for layer one
-    t_layer_input_input_size = t_layer_input_amount + 1;
-    t_layer_input_weight = function_XavierInitialization_For_ReLu(t_layer_input_input_size, t_layer_hidden_amount);
+    t_init_w2 = function_XavierInitialization_For_ReLu(t_n_input + 1, t_n_fc_hidden);
 
     %init the weight for layer two
-    t_layer_hidden_input_size = t_layer_hidden_amount + 1;
-    t_layer_hidden_weight = function_XavierInitialization_For_ReLu(t_layer_hidden_input_size, t_layer_output_amount);
+    t_init_w3 = function_XavierInitialization_For_ReLu( t_n_fc_hidden + 1, t_n_output);
 
     %pack the weigth together to compute the weight
-    t_packed_weight = [t_layer_input_weight(:); t_layer_hidden_weight(:)]; 
+    t_packed_init_w = [t_init_w2(:); t_init_w3(:)]; 
 
     %provide the layer one and layer two size
-    t_layer_input_weight_size = size(t_layer_input_weight);
-    t_layer_hidden_weight_size = size(t_layer_hidden_weight);
+    t_w2_size = size(t_init_w2);
+    t_w3_size = size(t_init_w3);
 
     %a hyper parameter of regularization param, close the regularization here
-    t_reularization_param = 0;
+    t_reg_param = 0;
 
     %assign the hyperparameter learning rate
     t_learning_rate = 0.01;
@@ -40,7 +38,7 @@ function[r_learnt_weight, r_cost_history, r_network_struct] = function_Learning_
     t_stochasitic_data_size = 50;
 
     %a storage for weigth in sgd 
-    t_packed_weight_sgd  = t_packed_weight;
+    t_learnt_w  = t_packed_init_w;
 
     %define the iteration time, 1000 * 100 times
     t_iteration_time = 10000;
@@ -53,24 +51,29 @@ function[r_learnt_weight, r_cost_history, r_network_struct] = function_Learning_
 
     %using momentum optimizer
     t_momentum_param = 0.9;
-    t_momentum_updater = zeros(size(t_packed_weight_sgd));
+    t_momentum_updater = zeros(size(t_learnt_w));
 
     %do gradient descent
     for i = 1: t_iteration_time
         
         %this will make the data always within [1:g_input_answer_amount]
-        t_rand_picked_data_index = floor(rand(1, t_stochasitic_data_size) * (t_training_data_amount - 1) ) + 1;
+        t_picked_index = floor(rand(1, t_stochasitic_data_size) * (t_n_all_data - 1) ) + 1;
         %generate stocastic data from dataset
-        t_rand_picked_data = p_training_data(t_rand_picked_data_index(1:end), :);
+        t_picked_X = p_training_data(t_picked_index(1:end), :);
         %provide the coresponding answer data as well
-        t_rand_picked_answer = p_training_answer(t_rand_picked_data_index(1:end), :);
+        t_picked_y = p_y(t_picked_index(1:end), :);
 
         %find the cost and gradient
-        [t_cost_param, t_gradient_param] = function_Compute_Cost_Gradient(t_packed_weight_sgd,t_rand_picked_data, t_rand_picked_answer, t_layer_input_weight_size, t_layer_hidden_weight_size, t_reularization_param);
+        [t_cost_param, t_gradient_param] = function_Compute_Cost_Gradient...
+            (...
+            t_learnt_w,t_picked_X, ...
+            t_picked_y, t_w2_size, ...
+            t_w3_size, t_reg_param...
+            );
         
        %compute two main update parameter for adam
         t_momentum_updater = t_momentum_param * t_momentum_updater + t_learning_rate * t_gradient_param;
-        t_packed_weight_sgd = t_packed_weight_sgd - t_momentum_updater;
+        t_learnt_w = t_learnt_w - t_momentum_updater;
         
         %output the cost to console every 100 iterate, so that we know
         %whether it is working, and the progress so far
@@ -83,14 +86,14 @@ function[r_learnt_weight, r_cost_history, r_network_struct] = function_Learning_
         
     end
     
-    r_learnt_weight = t_packed_weight_sgd;
+    r_learnt_weight = t_learnt_w;
     r_cost_history = t_record_cost_data;
     
     r_network_struct = struct(...
-        't_layer_input_amount', t_layer_input_amount,...
-        't_layer_hidden_amount', t_layer_hidden_amount,...
-        't_layer_output_amount', t_layer_output_amount,...
-        't_layer_input_weight_size', t_layer_input_weight_size,...
-        't_layer_hidden_weight_size', t_layer_hidden_weight_size...
+        't_layer_input_amount', t_n_input,...
+        't_layer_hidden_amount', t_n_fc_hidden,...
+        't_layer_output_amount', t_n_output,...
+        't_layer_input_weight_size', t_w2_size,...
+        't_layer_hidden_weight_size', t_w3_size...
         );
   
