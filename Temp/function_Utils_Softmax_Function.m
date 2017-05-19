7e69b16430f44a94a3c74bf698d0d4102dddf5d4
WyNfee <wangyifei52199@sina.com>
2017-05-20 00:09:03 +0800

    remove temporary files

--- Temp/function_Utils_Softmax_Function.m
+++
@@ -1,8 +0,0 @@
-%the softmax operation
-%the input data are thoese HAVE NOT been softmax-lized
-function r_softmax = function_Utils_Softmax_Function(p_input_data)
-    t_exponential = exp(p_input_data);
-    t_denominator = sum(t_exponential, 2);
-    t_softmax = bsxfun(@rdivide, t_exponential, t_denominator);
-    r_softmax = t_softmax;
-end
--- Temp/function_Utils_XavierInitializationForReLu.m
+++
@@ -1,9 +0,0 @@
-%Use Xavier's method to Initialize the weight; only for ReLu
-function r_weight = function_Utils_XavierInitializationForReLu(p_input_size, p_output_size)
-    %need to investigate the what the rand function is usually do, it
-    %should be normal distributed
-    t_weight = rand(p_input_size,p_output_size);
-    t_demoninator = sqrt(p_input_size/2);
-    r_weight = t_weight / t_demoninator;
- 
-end
