26925b8266213b21bab62c2c6ec611f67b84fd42
WyNfee <wangyifei52199@sina.com>
2017-05-19 23:03:15 +0800

    remove the batch normalization in this project

--- Utils/function_BatchNormalization.m
+++
@@ -1,9 +0,0 @@
-%do batch normalization for input matrix
-function r_bn = function_BatchNormalization(p_x)
-    epsilon = 1e-8;
-    t_mean_x = mean(mean(p_x));
-    t_std_x = std(p_x(:));
-    
-    r_bn = (p_x - t_mean_x) / (t_std_x + epsilon);
-    
-end
